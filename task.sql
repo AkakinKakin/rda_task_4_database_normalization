3d69bea4938e28f0c2810433f806782eba54d828
Eldar <eldardemchenko19@mail.com>
2025-06-25 02:36:11 +0300

    Solution

--- task.sql
+++ task.sql
@@ -9,25 +9,35 @@ CREATE TABLE Countries (
     PRIMARY KEY (ID)
 );
 
+CREATE TABLE ProductName(
+	ID INT,
+    ProductName VARCHAR(50),
+    PRIMARY KEY (ID)
+);
+
 CREATE TABLE ProductInventory (
     ID INT,
-    ProductName VARCHAR(50),
+    ProductNameID INT,
     WarehouseAmount INT,
     WarehouseName VARCHAR(50),
     WarehouseAddress VARCHAR(50), 
     CountryID INT,
+    FOREIGN KEY (ProductNameID) REFERENCES ProductName(ID) ON DELETE NO ACTION,
 	FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE NO ACTION,
     PRIMARY KEY (ID)
 );
 
 -- Populate test data
 
+INSERT INTO ProductName (ID, ProductName)
+VALUES (1, 'AwersomeProduct');
+
 INSERT INTO Countries (ID,Name)
 	VALUES (1, 'Country1');
 INSERT INTO Countries (ID,Name)
 	VALUES (2, 'Country2');
     
-INSERT INTO ProductInventory (ID,ProductName,WarehouseAmount,WarehouseName,WarehouseAddress,CountryID)
-	VALUES (1, 'AwersomeProduct', 2, 'Warehouse-1', 'City-1, Street-1',1);
-INSERT INTO ProductInventory (ID,ProductName,WarehouseAmount,WarehouseName,WarehouseAddress,CountryID)
-	VALUES (2, 'AwersomeProduct', 5, 'Warehouse-2', 'City-2, Street-2',2);
+INSERT INTO ProductInventory (ID,ProductNameID,WarehouseAmount,WarehouseName,WarehouseAddress,CountryID)
+	VALUES (1, 1, 2, 'Warehouse-1', 'City-1, Street-1',1);
+INSERT INTO ProductInventory (ID,ProductNameID,WarehouseAmount,WarehouseName,WarehouseAddress,CountryID)
+	VALUES (2, 1, 5, 'Warehouse-2', 'City-2, Street-2',2);
