c026285e54bf7aa53d96c2866197efa993b71c4b
Eldar <eldardemchenko19@mail.com>
2025-06-25 02:57:09 +0300

    Solution

--- task.sql
+++ task.sql
@@ -1,43 +1,37 @@
 -- Create database and tables
-
+DROP DATABASE IF EXISTS ShopDB;
 CREATE DATABASE ShopDB;
 USE ShopDB;
 
 CREATE TABLE Countries (
-    ID INT,
+    ID INT PRIMARY KEY,
+    Name VARCHAR(50)
+);
+
+CREATE TABLE Product(
+	ID INT PRIMARY KEY,
+    ProductName VARCHAR(50)
+);
+CREATE TABLE Warehouses(
+	ID INT PRIMARY KEY,
     Name VARCHAR(50),
-    PRIMARY KEY (ID)
-);
-
-CREATE TABLE ProductName(
-	ID INT,
-    ProductName VARCHAR(50),
-    PRIMARY KEY (ID)
-);
-
-CREATE TABLE ProductInventory (
-    ID INT,
-    ProductNameID INT,
-    WarehouseAmount INT,
-    WarehouseName VARCHAR(50),
-    WarehouseAddress VARCHAR(50), 
+    AddressID VARCHAR(50),
     CountryID INT,
-    FOREIGN KEY (ProductNameID) REFERENCES ProductName(ID) ON DELETE NO ACTION,
-	FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE NO ACTION,
-    PRIMARY KEY (ID)
+    FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE NO ACTION
+    
+);
+CREATE TABLE ProductInventory (
+    ID INT PRIMARY KEY,
+    ProductID INT,
+    Amount INT,
+    WarehousesID INT,
+    FOREIGN KEY (WarehousesID) REFERENCES Warehouses(ID) ON DELETE NO ACTION, 
+    FOREIGN KEY (ProductID) REFERENCES Product(ID) ON DELETE NO ACTION
 );
 
 -- Populate test data
 
-INSERT INTO ProductName (ID, ProductName)
-VALUES (1, 'AwersomeProduct');
+INSERT INTO Countries (ID, Name) VALUES (1, 'Country1');
+INSERT INTO Countries (ID, Name) VALUES (2, 'Country2');
 
-INSERT INTO Countries (ID,Name)
-	VALUES (1, 'Country1');
-INSERT INTO Countries (ID,Name)
-	VALUES (2, 'Country2');
-    
-INSERT INTO ProductInventory (ID,ProductNameID,WarehouseAmount,WarehouseName,WarehouseAddress,CountryID)
-	VALUES (1, 1, 2, 'Warehouse-1', 'City-1, Street-1',1);
-INSERT INTO ProductInventory (ID,ProductNameID,WarehouseAmount,WarehouseName,WarehouseAddress,CountryID)
-	VALUES (2, 1, 5, 'Warehouse-2', 'City-2, Street-2',2);
+INSERT INTO Product(ID, ProductName) VALUES (1, 'AwersomeProduct');
