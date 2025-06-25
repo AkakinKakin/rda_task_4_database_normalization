fd8ccb87ed44c9ebba1c73dac07b2e2c050f546f
Eldar <eldardemchenko19@mail.com>
2025-06-25 03:12:15 +0300

    fixed2

--- task.sql
+++ task.sql
@@ -15,7 +15,7 @@ CREATE TABLE Product(
 CREATE TABLE Warehouses(
 	ID INT PRIMARY KEY,
     Name VARCHAR(50),
-    AddressID VARCHAR(50),
+    Address VARCHAR(50),
     CountryID INT,
     FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE NO ACTION
     
@@ -23,9 +23,9 @@ CREATE TABLE Warehouses(
 CREATE TABLE ProductInventory (
     ID INT PRIMARY KEY,
     ProductID INT,
+    WarehouseID INT,
     Amount INT,
-    WarehousesID INT,
-    FOREIGN KEY (WarehousesID) REFERENCES Warehouses(ID) ON DELETE NO ACTION, 
+    FOREIGN KEY (WarehouseID) REFERENCES Warehouses(ID) ON DELETE NO ACTION, 
     FOREIGN KEY (ProductID) REFERENCES Product(ID) ON DELETE NO ACTION
 );
 
@@ -35,3 +35,11 @@ INSERT INTO Countries (ID, Name) VALUES (1, 'Country1');
 INSERT INTO Countries (ID, Name) VALUES (2, 'Country2');
 
 INSERT INTO Product(ID, ProductName) VALUES (1, 'AwersomeProduct');
+
+INSERT INTO Warehouses (ID, Name, Address, CountryID) VALUES (1, 'Warehouse-1', 'Street-1 City-1', 1);
+INSERT INTO Warehouses (ID, Name, Address, CountryID) VALUES (2, 'Warehouse-2', 'Street-1 City-2', 2);
+
+INSERT INTO ProductInventory (ID, ProductID, WarehouseID, Amount)
+VALUES 
+    (1, 1, 1, 2),
+    (2, 1, 2, 5);
